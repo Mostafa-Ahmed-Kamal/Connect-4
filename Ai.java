5d952042b7ec672d6fccdb9c59878c237b51d365
Elazhaby <Elzahaby31@Gmail.com>
2021-12-06 18:10:15 +0200

    Elzahaby's commit

--- Ai.java
+++ Ai.java
@@ -1,17 +1,138 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Random;
 
 public class Ai {
-	
+
+	private static int MAX_DEPTH;
+	private byte[][] Board;
+	private HashMap<String, Integer> explored;
+
 	//without pruning
-	public State miniMax(State state) {
-		return null;
+	public Solution miniMax(byte[][] board, int maxDepth){
+		MAX_DEPTH = maxDepth;
+		Board = board;
+		// root.
+		BoardState root = new BoardState(Board, true);
+		root.setMarked(true);
+		explored = new HashMap<>();
+		BoardState bestChild = maximize(root, 1);
+		int[] choice = mark(bestChild);
+		return new Solution(root, choice);
 	}
+
+	private BoardState maximize(BoardState state, int depth){
+		// Terminal state.
+		// 1. max depth is reached.
+		// 2. Board is full.
+		// 3. state already visited.
+		if (depth == MAX_DEPTH || isFull()){
+			Board[state.getRow()][state.getCol()] = 2;
+			int val = Heuristic(state);
+			state.setValue(val);
+			Board[state.getRow()][state.getCol()] = 0;
+			return state;
+		}
+		depth++;
+		BoardState maxChild = new BoardState(null, false);
+		maxChild.setValue(Integer.MIN_VALUE);
+		for (BoardState child : state.calcChildren(Board)) {
+			if (explored.containsKey(child.getID()))
+				continue;
+
+			Board[child.getRow()][child.getCol()] = 2;
+			BoardState temp = minimize(child, depth);
+
+			explored.put(child.getID(), child.getValue());
+
+			Board[child.getRow()][child.getCol()] = 0;
+			if (temp.getValue() > maxChild.getValue())
+				maxChild = temp;
+		}
+		state.setValue(maxChild.getValue());
+		return maxChild;
+	}
+
+	private BoardState minimize(BoardState state, int depth){
+		if (depth == MAX_DEPTH || isFull()){
+			Board[state.getRow()][state.getCol()] = 1;
+			int val = Heuristic(state);
+			state.setValue(val);
+			Board[state.getRow()][state.getCol()] = 0;
+			return state;
+		}
+		depth++;
+		BoardState minChild = new BoardState(null, true);
+		minChild.setValue(Integer.MAX_VALUE);
+		for (BoardState child : state.calcChildren(Board)) {
+			if (explored.containsKey(child.getID()))
+				continue;
+
+			Board[child.getRow()][child.getCol()] = 1;
+			BoardState temp = maximize(child, depth);
+
+			explored.put(child.getID(), child.getValue());
+
+			Board[child.getRow()][child.getCol()] = 0;
+			if (temp.getValue() < minChild.getValue())
+				minChild = temp;
+		}
+		state.setValue(minChild.getValue());
+		return minChild;
+	}
+
+	private boolean isFull(){
+		boolean flag = true;
+		for (int i = 0; i < 7; i++)
+			flag = flag && Board[0][i] != 0;
+		return flag;
+	}
+
+
+	// 1. get Children.
+	// 2. change heuristic.
+
 	//with pruning
 	public State miniMax(State state, int alpha , int beta) {
 		return null;
 	}
 	
-	public int Heuristic() {
-		return 0;
+	private int Heuristic(BoardState state) {
+		Random r = new Random();
+		return r.nextInt(100);
 	}
-	
+
+	private int[] mark(BoardState state){
+		int i = 0, j = 0;
+		while (state.getParent() != null){
+			state.setMarked(true);
+			i = state.getRow();
+			j = state.getCol();
+			state = state.getParent();
+		}
+		return new int[]{i, j};
+	}
+
+	// level order.
+	public void print(BoardState root){
+		Queue<BoardState> queue = new LinkedList<>();
+		queue.add(root);
+		while (!queue.isEmpty()) {
+			BoardState temp = queue.poll();
+			System.out.println(temp.getValue());
+			if (temp.getChildren() == null)
+				continue;
+			queue.addAll(temp.getChildren());
+		}
+	}
+
+	public static void main(String[] args) {
+		byte[][] board = {{0,0,0,0,0,0,0}, {0,0,2,0,0,0,0}, {1,2,1,2,1,2,0}, {1,1,1,2,2,1,0},
+				{1,2,2,1,1,2,0}, {2,1,1,2,2,2,1}};
+		Ai ai = new Ai();
+		Solution sol = ai.miniMax(board, 4);
+		System.out.println("(" + sol.getChoice()[0] + "," + sol.getChoice()[1] + ")");
+		System.out.println(sol.getRoot().getValue());
+	}
 }
--- BoardState.java
+++ BoardState.java
@@ -1,20 +1,134 @@
 import java.util.LinkedList;
 
-public class BoardState implements State{
+public class BoardState {
 
-	int addedRow;
-	int addedColumn;
-	State parent;
-	
-	@Override
-	public void setChildren() {
-		// TODO Auto-generated method stub
-		
+	private static final char[] DIGITS = {'0', '1', '2', '3', '4', '5', '6', '7', '8','9', 'A', 'B', 'C',
+			'D', 'E', 'F', 'G', 'H','I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q'};
+
+	private int row;
+	private int col;
+	// TODO.
+	private BoardState parent;
+	private LinkedList<BoardState> children;
+
+	private int value;
+	private String ID;
+	private boolean isMax;
+	private boolean isMarked;
+
+	public boolean isMarked() {
+		return isMarked;
 	}
-	@Override
-	public LinkedList<State> getChildren() {
-		// TODO Auto-generated method stub
-		return null;
+
+	public void setMarked(boolean marked) {
+		isMarked = marked;
 	}
-	
+
+	public BoardState (byte[][] board, boolean isMax){
+		ID = calcID(board);
+		row = 0;
+		col = 0;
+		value = 0;
+		this.isMax = isMax;
+		isMarked = false;
+	}
+
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+	public String getID() {
+		return ID;
+	}
+
+	public LinkedList<BoardState> calcChildren(byte[][] board) {
+		if (board == null)
+			return null;
+		LinkedList<BoardState> children = new LinkedList<>();
+		for (int j = 0; j < 7; j++) {
+			if (board[0][j] != 0)
+				continue;
+			BoardState child;
+			for (int i = 1; i < 6; i++) {
+				if (board[i][j] != 0){
+					if (isMax)
+						board[i-1][j] = 2;
+					else
+						board[i-1][j] = 1;
+					child = new BoardState(board, !isMax);
+					child.setRow(i-1);
+					child.setCol(j);
+					child.setParent(this);
+					children.add(child);
+
+					// reset.
+					board[i-1][j] = 0;
+					break;
+				}
+				else if (i == 5){
+					if (isMax)
+						board[i][j] = 2;
+					else
+						board[i][j] = 1;
+					child = new BoardState(board, !isMax);
+					child.setRow(i);
+					child.setCol(j);
+					child.setParent(this);
+					children.add(child);
+
+					// reset.
+					board[i][j] = 0;
+				}
+			}
+		}
+		this.children = children;
+		return children;
+	}
+
+	private String calcID(byte[][] board){
+		if (board == null)
+			return null;
+		StringBuilder idBuilder = new StringBuilder();
+		int index = 0;
+		for (int j = 0; j < 7; j++) {
+			for (int i = 0; i < 6; i += 3) {
+				index += board[i][j] + board[i+1][j] * 3 + board[i+2][j] * 9;
+				idBuilder.append(DIGITS[index]);
+				index = 0;
+			}
+		}
+		return idBuilder.toString();
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	public void setRow(int row) {
+		this.row = row;
+	}
+
+	public int getCol() {
+		return col;
+	}
+
+	public void setCol(int col) {
+		this.col = col;
+	}
+
+	public BoardState getParent() {
+		return parent;
+	}
+
+	public void setParent(BoardState parent) {
+		this.parent = parent;
+	}
+
+	public LinkedList<BoardState> getChildren(){
+		return children;
+	}
 }
