aab44e4567c049562705e75c946fc3185e846d0d
Mostafa Ahmed Kamal Ahmed Mesbah <61250718+Mostafa-Ahmed-Kamal@users.noreply.github.com>
2021-12-09 04:40:58 +0200

    Delete State.java

--- State.java
+++
@@ -1,9 +0,0 @@
-import java.util.LinkedList;
-
-public interface State{
-	
-	LinkedList<State> children = new LinkedList<State>();
-	
-	public void setChildren();
-	public LinkedList<State> getChildren();
-}
